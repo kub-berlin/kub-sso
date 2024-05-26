9e5c0e8f03fd2a16f9ad865448c40254a3ae931b
Tobias Bengfort <tobias.bengfort@posteo.de>
2024-05-26 20:50:20 +0200

    oidc: extend

--- server/oidc.py
+++ server/oidc.py
@@ -1,4 +1,4 @@
-# https://www.rfc-editor.org/rfc/rfc6749
+# https://datatracker.ietf.org/doc/draft-ietf-oauth-v2-1/
 # https://openid.net/specs/openid-connect-core-1_0.html
 
 import datetime
@@ -21,7 +21,9 @@ def utcnow():
 def update_url(url: str, **params) -> str:
     url_parts = list(urllib.parse.urlparse(url))
     query = urllib.parse.parse_qs(url_parts[4])
-    query.update(params)
+    for key, value in params.items():
+        if value is not None:
+            query[key] = value
     url_parts[4] = urllib.parse.urlencode(query)
     return urllib.parse.urlunparse(url_parts)
 
@@ -39,11 +41,14 @@ def get_code(encoded: str, client_id: str, config: dict) -> dict:
     return jwt.decode(encoded, secret, audience=client_id, algorithms=['HS256'])
 
 
-def login_validate(request, config: dict) -> bool:
-    return (
-        request.query.get('response_type') == 'code'
-        and request.query.get('client_id') in config['clients']
-    )
+def generate_id_token(client_id: str, username: str, config: dict) -> str:
+    return jwt.encode({
+        'iss': config['server']['issuer'],
+        'sub': username,
+        'aud': client_id,
+        'exp': utcnow() + datetime.timedelta(seconds=20),
+        'iat': utcnow(),
+    }, config['server']['secret'], algorithm='HS256')
 
 
 def render_form(request, *, error: bool):
@@ -57,8 +62,18 @@ def render_form(request, *, error: bool):
 async def login_handler(request):
     config = request.app['config']
 
-    if not login_validate(request, config):
-        raise web.HTTPBadRequest
+    try:
+        if 'openid' not in request.query['scope'].split():
+            raise ValueError
+        if request.query['response_type'] != 'code':
+            raise ValueError
+        client_id = request.query['client_id']
+        client = config['clients'][client_id]
+        redirect_uri = request.query['redirect_uri']
+        if redirect_uri != client['redirect_uri']:
+            raise ValueError
+    except Exception as e:
+        raise web.HTTPBadRequest from e
 
     if request.method != 'POST':
         return render_form(request, error=False)
@@ -73,21 +88,14 @@ async def login_handler(request):
     user = await backends.auth(username, password, config)
     if not user:
         return render_form(request, error=True)
-    elif request.query['client_id'] not in user.get('clients', []):
+    elif client_id not in user.get('clients', []):
         raise web.HTTPForbidden
     else:
-        client_id = request.query['client_id']
-        client = config['clients'][client_id]
-        response = web.Response(status=302, headers={'Location': update_url(
-            client['redirect_uri'],
-            state=request.query.get('state', ''),
-            code=generate_code(
-                request.query['client_id'],
-                username,
-                config,
-            ),
+        return web.Response(status=302, headers={'Location': update_url(
+            redirect_uri,
+            state=request.query.get('state'),
+            code=generate_code(client_id, username, config),
         )})
-        return response
 
 
 async def token_handler(request):
@@ -120,6 +128,7 @@ async def token_handler(request):
     return web.json_response({
         'access_token': 'noop',
         'token_type': 'Bearer',
+        'id_token': generate_id_token(client_id, username, config),
         'name': user.get('full_name'),
         'email': user.get('email'),
     }, headers={
