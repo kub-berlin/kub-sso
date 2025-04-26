3f956c304df1a71649b53cd5b2ac2097be7deb36
Tobias Bengfort <tobias.bengfort@posteo.de>
2025-04-26 23:18:47 +0200

    lock users if they haven't logged in for too long

--- server/backends.py
+++ server/backends.py
@@ -1,10 +1,15 @@
 import asyncio
+import datetime
+import json
 import logging
 import sys
 from getpass import getpass
+from pathlib import Path
 
 import aiosmtplib
 import argon2
+
+LAST_LOGIN_PATH = Path('/var/lib/kub-sso/last_login.json')
 
 logger = logging.getLogger(__name__)
 hasher = argon2.PasswordHasher()
@@ -67,6 +72,26 @@ async def check_smtp_password(email, password, config):
         return False
 
 
+def check_last_login(username, config):
+    if LAST_LOGIN_PATH.exists():
+        with LAST_LOGIN_PATH.open() as fh:
+            data = json.load(fh)
+    else:
+        data = {}
+
+    today = datetime.date.today()
+    s = data.get(username)
+    if s:
+        last_login = datetime.date.fromisoformat(s)
+        days = config['server'].get('max_days_since_last_login', 30)
+        if last_login + datetime.timedelta(days=days) < today:
+            raise ValueError('last_login')
+
+    data[username] = today.isoformat()
+    with LAST_LOGIN_PATH.open('w') as fh:
+        json.dump(data, fh, indent=2, sort_keys=True)
+
+
 async def _auth(username, password, config):
     if username not in config['users']:
         return None
@@ -89,7 +114,11 @@ async def _auth(username, password, config):
 
 async def auth(username, password, config):
     try:
-        return await _auth(username, password, config)
+        user = await _auth(username, password, config)
+        if user:
+            check_last_login(username, config)
+        return user
     except Exception:
         logger.exception('Authentication failed')
         return None
+    return user
