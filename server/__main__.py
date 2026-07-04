34aa1f296a2999b31ec932ae108c9407bb5b73d6
Tobias Bengfort <tobias.bengfort@posteo.de>
2026-07-04 06:35:35 +0200

    rm unused PAM handler

--- server/__main__.py
+++ server/__main__.py
@@ -9,31 +9,6 @@ from . import last_login
 from . import oidc
 from . import signup
 
-
-async def pam_handler(request):
-    config = request.app['config']
-    post_data = await request.post()
-
-    try:
-        username = post_data['username']
-        password = post_data['password']
-    except KeyError as e:
-        raise web.HTTPBadRequest from e
-
-    user_config = await backends.auth(username, password, config)
-
-    if not user_config:
-        raise web.HTTPForbidden
-
-    return web.json_response({
-        'username': username,
-        'uid': user_config['unix_id'],
-        'full_name': user_config.get('full_name', ''),
-        'groups': user_config.get('unix_groups', []),
-        'ecryptfs_passphrase': user_config['ecryptfs_passphrase'],
-    })
-
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--port', type=int, default=8000)
@@ -55,8 +30,6 @@ if __name__ == '__main__':
     app['dir'] = Path(__file__).parent
     with open(args.config, 'rb') as fh:
         app['config'] = tomllib.load(fh)
-
-    app.router.add_post('/pam/', pam_handler)
 
     app.router.add_get('/signup/', signup.signup_handler)
     app.router.add_post('/signup/', signup.signup_handler)
